73f0ef55f76ed27b16eb9a6a578c53b1f8df0abb
Hannah Barton <hannbarton@gmail.com>
2018-12-05 07:36:08 -0500

    added fadding name at top

--- header.js
+++ header.js
@@ -1,10 +1,8 @@
 const headerTag = document.querySelector("header")
 
-// when we scroll the page, at a certain point (80px)
-// toggle a class to the header
 document.addEventListener("scroll", function () {
   const pixels = window.pageYOffset
-  
+
   if (pixels > 80) {
     headerTag.classList.add("scrolled")
   } else {
--- stamps.js
+++ stamps.js
@@ -17,17 +17,11 @@ const addStamp = function (x, y) {
   const img = document.createElement("img")
   img.setAttribute("src", stamps[number])
 
-  // remove half the window width
   img.style.left = (x - window.innerWidth / 2) + "px"
   img.style.top = y + "px"
   img.style.width = "55px"
 
   stampsTag.appendChild(img)
-
-  // add some audio
-  // const audio = document.createElement("audio")
-  // audio.setAttribute("src", "assets/plop.mp3")
-  // audio.play()
 
   number = number + 1
   if (number > stamps.length - 1) {
