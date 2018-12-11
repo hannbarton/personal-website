4107510def09a88dc20296130053c9981186fad4
Hannah Barton <hannbarton@gmail.com>
2018-12-11 11:42:24 -0500

    added bcard file

---
+++ hannbarton.js
@@ -0,0 +1,13 @@
+const name = 'Hannah Barton';
+const message = `hello ${you}!
+I am a full stack developer`;
+
+const contact = {
+    website: 'hannbarton.com',
+    email: 'hannbarton@gmail.com',
+    github: 'github.com/hannbarton',
+};
+
+
+
+
